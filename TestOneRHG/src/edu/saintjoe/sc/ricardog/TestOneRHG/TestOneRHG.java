5151b4562422ef3573cdface466e630f17a54645
ricardog <ricardog@localhost>
2016-02-26 15:26:31 -0500

    Final Commit

--- TestOneRHG/src/edu/saintjoe/sc/ricardog/TestOneRHG/TestOneRHG.java
+++ TestOneRHG/src/edu/saintjoe/sc/ricardog/TestOneRHG/TestOneRHG.java
@@ -3,9 +3,17 @@ package edu.saintjoe.sc.ricardog.TestOneRHG;
 public class TestOneRHG {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		int nameNum = 6;
+		while (nameNum > 0) {
+			String word = "Ricardo Gutierrez";
+			System.out.println(nameNum + " " + word + " :) ");
 
+			nameNum = nameNum - 1;
+			if (nameNum == 1) { 
+				word = "Ricardo Gutierrez";
+			}	
+		}	
+		
 	}
 
 }
